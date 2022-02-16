e57b90709b776fc86cf09de06e8e940a15f0a22d
Art'S <94591965+plzZarbotay@users.noreply.github.com>
2022-02-16 21:50:14 +0300

    Create pyt.py

---
+++ pyt.py
@@ -0,0 +1,12 @@
+print("Система LOS! \n")
+print("Введите ваш ник \n")
+name = ""
+name = str(input("*Ожидание*: "))
+print("Введите название программы \n")
+print("1- Calc \n")
+arg = ""
+arg = str(input("*Ожидание*: "))
+if(arg == "Calc"):
+    print("Введите название программы \n")
+print(name)
+
