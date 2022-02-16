2bd6a51c004ba2d32b97c5c8ee9bd29b44f53b83
Art'S <94591965+plzZarbotay@users.noreply.github.com>
2022-02-16 17:52:15 +0300

    Create humanstats.rs

---
+++ humanstats.rs
@@ -0,0 +1,18 @@
+use std::io;
+ 
+fn main()  -> io::Result<()>{
+
+    let mut input = String::new();
+     let mut inputage =String::new();
+     let mut project = String::new();
+    println!("Введите свое имя: ");
+    io::stdin().read_line(&mut input)?;
+    println!("Введите ваш возраст: ");
+    io::stdin().read_line(&mut inputage);
+    println!("Введите ссылку на ваш код");
+    io::stdin().read_line(&mut project);
+    println!("Ваш имя: {}", input);
+    println!("Ваш возраст: {}", inputage);
+     println!("Ссылка на ваш код: {}", project);
+    Ok(())
+}
