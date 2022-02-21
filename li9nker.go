63459f28bd8cac8b9d78fd4e939ab70a473039c3
Art'S <94591965+plzZarbotay@users.noreply.github.com>
2022-02-21 16:45:59 +0300

    Create li9nker.go

---
+++ li9nker.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Проект <LemCom>")
+	var id uint64 = 0	//int:
+  fmt.Scanf("Open Source", &id)
+  fmt.Println("Отлично! Теперь мы знаем ваш ID!)
+  
+  
+  
+  
+  
+  
+  
+  
+}
