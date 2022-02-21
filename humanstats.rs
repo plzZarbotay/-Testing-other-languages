fdbfffa807182e92a7ed9c466a1e15eb994b70a3
Art'S <94591965+plzZarbotay@users.noreply.github.com>
2022-02-21 16:41:24 +0300

    Update humanstats.rs

--- humanstats.rs
+++ humanstats.rs
@@ -5,15 +5,19 @@ fn main()  -> io::Result<()>{
     let mut input = String::new();
      let mut inputage =String::new();
      let mut project = String::new();
+      let mut ling = String::new();
     println!("Введите свое имя: ");
     io::stdin().read_line(&mut input)?;
     println!("Введите ваш возраст: ");
     io::stdin().read_line(&mut inputage);
     println!("Введите ссылку на ваш код");
     io::stdin().read_line(&mut project);
+  println!("Введите Ваши данные:");
+    io::stdin().read_line(&mut ling);
     println!("Ваш имя: {}", input);
     println!("Ваш возраст: {}", inputage);
      println!("Ссылка на ваш код: {}", project);
+     println!("Ссылка на ваш код: {}", ling);
     Ok(())
 }
-//fpr
+
